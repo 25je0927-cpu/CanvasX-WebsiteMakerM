d6f2aa30a33b63ecdb8352d744846ed9e7bd39cc
25je0927-cpu <25je0927@iitism.ac.in>
2026-01-07 17:15:03 +0530

    Add Navbar component with user authentication links

---
+++ src/components/navbar.js
@@ -0,0 +1,75 @@
+// components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar({ userDetails, onLogout }) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        {/* Brand */}
+        <Link className="navbar-brand" to="/builder">
+          CanvasX
+        </Link>
+
+        {/* Toggle for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Profile dropdown on right */}
+          {userDetails ? (
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="profileDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {userDetails.firstName || "User"}
+                </a>
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="profileDropdown"
+                >
+                  <li>
+                    <Link className="dropdown-item" to="/profile">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <button className="dropdown-item" onClick={onLogout}>
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
